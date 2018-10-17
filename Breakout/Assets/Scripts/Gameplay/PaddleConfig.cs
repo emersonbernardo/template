74ab209235adf0ecea48a801509e505c14b1e0d0
emersonbernardo <44222307+emersonbernardo@users.noreply.github.com>
2018-10-17 17:49:30 +0800

    + Added Paddle gameobjects and scripts.

---
+++ Breakout/Assets/Scripts/Gameplay/PaddleConfig.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleConfig : ScriptableObject {
+    public float speed;
+    public float rightEdgeScreen;
+    public float leftEdgeScreen;
+}
---
+++ Breakout/Assets/Scripts/Gameplay/PaddleScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour {
+
+    [SerializeField]
+    private PaddleConfig paddleConfig;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector2.right * horizontal * Time.deltaTime * paddleConfig.speed);
+        if (transform.position.x < paddleConfig.leftEdgeScreen)
+        {
+            transform.position = new Vector2(paddleConfig.leftEdgeScreen, transform.position.y);
+        }
+
+        if (transform.position.x > paddleConfig.rightEdgeScreen)
+        {
+            transform.position = new Vector2(paddleConfig.rightEdgeScreen, transform.position.y);
+        }
+    }
+}
