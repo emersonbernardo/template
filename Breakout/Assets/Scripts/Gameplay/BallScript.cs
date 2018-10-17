2f92d5a4da8c404b3f3803d1633d069a510e21fb
emersonbernardo <44222307+emersonbernardo@users.noreply.github.com>
2018-10-17 18:20:37 +0800

    + Added ball object.

---
+++ Breakout/Assets/Scripts/Gameplay/BallScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.AddForce(Vector2.up * 500);
+    }
+
+    private void Update()
+    {
+        
+    }
+}
