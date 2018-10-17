1dee33e91853b679d9436b31b3441404c850ef85
emersonbernardo <44222307+emersonbernardo@users.noreply.github.com>
2018-10-17 20:48:14 +0800

    + Added UI

--- Breakout/Assets/Scripts/Gameplay/BallScript.cs
+++ Breakout/Assets/Scripts/Gameplay/BallScript.cs
@@ -5,16 +5,30 @@ using UnityEngine;
 public class BallScript : MonoBehaviour {
 
     private Rigidbody2D rb;
+    private bool inPlay;
+    [SerializeField]
+    private Transform paddle;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-
         rb.AddForce(Vector2.up * 500);
     }
 
     private void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("BottomCollider"))
+        {
+            Debug.Log("Ball hit the bottom of the screen");
+        }
     }
 }
